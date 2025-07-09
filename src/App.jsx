bab2210036180b1ab4c8b4fcdeecd5d650d3f2ed
Noor Safia <nor.safieh@gmail.com>
2025-07-09 10:57:42 +0300

    Initial setup for AOS scroll animations (library installed and styles imported)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import {Navbar} from './components/navbar'
 import {Footer} from './components/footer'
 import Homepage from './pages/homepage'
 import 'leaflet/dist/leaflet.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 function App() {
   
