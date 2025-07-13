97306919d3755eee13eac39972cabd7b5815dfbf
Noor Safia <nor.safieh@gmail.com>
2025-07-13 11:19:44 +0300

    Delete comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import {Navbar} from './components/navbar'
 import {Footer} from './components/footer'
 import Homepage from './pages/homepage'
-// import Mainpage from './pages/mainpage'
 import 'leaflet/dist/leaflet.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -11,12 +10,6 @@ import Mainpage from "./pages/mainpage";
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 
 function App() {
-  // useEffect(() => {
-  //   AOS.init({
-  //     duration: 1500, 
-  //     once: true      
-  //   });
-  // }, []);
 
   return (
     <>
